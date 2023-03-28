3c345333a1f9cc4bfad73094be30581796054def
mmahadevisingh <78149372+mmahadevisingh@users.noreply.github.com>
2023-03-28 09:33:33 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,4 @@
+x=parseFloat(prompt("Enter 1st no."));
+y=parseFloat(prompt("Enter 2nd no."));
+z=x+y;
+document.write("sum="+z);
---
+++ js2.js
@@ -0,0 +1,6 @@
+x=parseFloat(prompt("1. Enter a no."));
+if(x%2==0)
+clr="red";
+else
+clr="green";
+document.write("<h1 style='color:"+clr+"'>"+x+"</h1>");
